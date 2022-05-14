6ff160988b5ee88473fa0354b601c15949ac3a59
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2022-05-14 14:17:11 +0430

    Removed unused imports

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 import os
-import socket
-# import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
